32843b2789ea690d936af853a46d63f6e94c48a4
Samuel Ribeiro Batista <leidymotastz@outlook.com>
2022-08-24 18:24:56 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicao
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            #region Condicao Encadeada
+
+            //int valor =15;
+
+            //if (valor > 5)
+
+            //{
+            //    Console.WriteLine("Condição verdadeira");
+            //}
+
+            //else if (valor >= 5 && valor < 10)
+            //{
+
+            //    Console.WriteLine("Condição alternativa 1 ");
+
+            //}
+            //else if (valor >= 10 && valor < 20 )
+
+            //{ 
+            //  Console.WriteLine("Condição alternativa 2 ");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Condição alternativa final ");
+            //}
+
+            #endregion
+
+            #region Condicao Aninhada
+
+            //Console.WriteLine("Digite um numero.");
+
+            //int numero = Int32.Parse(Console.ReadLine());
+
+            //if (numero > 5)
+            //{
+            //    Console.Write("o numero e maior que 5");
+            //}           
+            //if (numero % 2 == 0)
+            //{
+            //    Console.WriteLine("é tambem é par.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("mas nao e par.");
+            //}
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Operador Tinerario
+
+                        
+            //int numero = 3;
+            //string menssagem = "";
+
+            //// Condicao ? true : false;
+            // menssagem = numero > 5 ? "Maior que 5" : "Menor que 5";
+
+
+
+            ////if (numero > 5 )
+            ////{
+            ////    menssagem = "maior que 5";
+            ////}
+            ////else
+            ////{
+            ////    menssagem = "menor que 5";
+            ////}
+
+
+
+            //Console.WriteLine(menssagem);
+            //Console.ReadKey();
+
+            #endregion
+        }
+
+
+
+
+
+
+
+
+
+    } 
+
+}
+                               
+
+
+                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
